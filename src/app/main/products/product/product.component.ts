74073e772427e8fe8dadb87575b1b7052bfbe3c7
Wojciech Kwiatek <wojtek.kwiatek@gmail.com>
2017-08-05 12:34:37 +0200

    Initial commit

---
+++ src/app/main/products/product/product.component.ts
@@ -0,0 +1,19 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { IProduct } from '../../../../shared/models/product';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss']
+})
+export class ProductComponent implements OnInit {
+
+  @Input() product: IProduct;
+  @Output() buyProductClick = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
