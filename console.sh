18495808055d6d666eb9c152537f4ff0787be540
Wojciech Kwiatek <wojtek.kwiatek@gmail.com>
2017-08-05 14:05:27 +0200

    Initial commit

--- console.sh
+++
@@ -1,4 +0,0 @@
-# We can generate components using ng cli
-ng g c main
-ng g c main/products
-ng g c main/products/product
--- src/shared/models/product.ts
+++
@@ -1,6 +0,0 @@
-//3/ Interface that keeps the shape of values will be helpful
-export interface IProduct {
-  id: number | string;
-  name: string;
-  price: number;
-}
