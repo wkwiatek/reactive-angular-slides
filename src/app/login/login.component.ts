932a8a8e476b31456990ba87cd68c052a1bf4df5
Wojciech Kwiatek <wojtek.kwiatek@gmail.com>
2017-08-09 00:00:33 +0200

    [Store for Features] Adding part of store for specific features

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { getProducts, IState } from '../shared/reducers/index';
+import { ILoginState, isFormVisible } from './reducers/index';
+import { Observable } from 'rxjs/Observable';
+import { IProduct } from '../shared/models/product';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +12,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public products$: Observable<IProduct[]>;
+  public isFormVisible$: Observable<boolean>;
+
+  // We can inject store as we always did
+  constructor(private store: Store<IState | ILoginState>) {
+    //2/ And we're getting access to both parts - login, and products
+    this.products$ = store.select(getProducts);
+    this.isFormVisible$ = store.select(isFormVisible);
+  }
 
   ngOnInit() {
   }
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -3,12 +3,15 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { LoginComponent } from './login.component';
 import { loginRoutes } from './login.routes';
+import { StoreModule } from '@ngrx/store';
+import { loginReducer } from './reducers/index';
 
-//9/ Login module with routing
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forChild(loginRoutes)
+    RouterModule.forChild(loginRoutes),
+    // Store module has also option to add for specific module
+    StoreModule.forFeature('login', loginReducer)
   ],
   declarations: [LoginComponent]
 })
