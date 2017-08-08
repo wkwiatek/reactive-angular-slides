023d964129c47fa168a208aec00dc8c79e7352b2
Wojciech Kwiatek <wojtek.kwiatek@gmail.com>
2017-08-08 18:50:07 +0200

    [Dispatch test] Testing dispatch

--- src/app/main/main.component.spec.ts
+++ src/app/main/main.component.spec.ts
@@ -1,16 +1,29 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MainComponent } from './main.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { IState } from '../shared/reducers/index';
 
-describe('MainComponent', () => {
+fdescribe('MainComponent', () => {
   let component: MainComponent;
   let fixture: ComponentFixture<MainComponent>;
+  let store: Store<IState>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MainComponent ]
+      declarations: [ MainComponent ],
+      //4/ Mock store and dispatch, select functions
+      providers: [{
+        provide: Store,
+        useValue: jasmine.createSpyObj('Store', ['dispatch', 'select']),
+      }],
+      // Tells the compiler not to error on unknown elements and attributes aka shallow render
+      schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
+
+    store = TestBed.get(Store);
   }));
 
   beforeEach(() => {
@@ -21,5 +34,10 @@ describe('MainComponent', () => {
 
   it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should dispatch action when encountered buy click event', () => {
+    component.handleBuyProduct('id');
+    expect(store.dispatch).toHaveBeenCalled();
   });
 });
--- src/app/shared/effects/products.spec.ts
+++ src/app/shared/effects/products.spec.ts
@@ -2,13 +2,11 @@ import { ProductsEffects } from './products';
 import { Observable } from 'rxjs/Observable';
 import { TestBed } from '@angular/core/testing';
 import { provideMockActions } from '@ngrx/effects/testing';
-import { ReplaySubject } from 'rxjs/ReplaySubject';
 import { hot, cold } from 'jasmine-marbles';
 import { asyncGetProducts } from '../actions/products';
 import { ProductsService } from "../services/products.service";
 
 describe('[Effects]', () => {
-  //3/ Here we can define helpers that will be needed across tests
   let effects: ProductsEffects;
   let actions$: Observable<any>;
   let productsService: any;
@@ -16,11 +14,8 @@ describe('[Effects]', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
-        // First, effects that we're about to test
         ProductsEffects,
-        // Mock actions
         provideMockActions(() => actions$),
-        //4/ Mocked service (the one that is calling API)
         {
           provide: ProductsService,
           useValue: jasmine.createSpyObj('ProductsService', ['getProducts']),
@@ -33,7 +28,6 @@ describe('[Effects]', () => {
   });
 
   it('should work', () => {
-    //7/ Action that initializes effect, and the one that's expected to be dispatched
     const initAction = asyncGetProducts.init();
     const products = [{
       id: 0,
@@ -42,15 +36,12 @@ describe('[Effects]', () => {
     }];
     const completeAction = asyncGetProducts.success(products);
 
-    //3/ Marbles for action, response, and expected value
     actions$ = hot('-a---', { a: initAction });
     const response = cold('---b|', { b: products });
     const expected = cold('----c', { c: completeAction });
 
-    // Value to respond with
     productsService.getProducts.and.returnValue(response);
 
-    // Eventually, the check
     expect(effects.getProducts$).toBeObservable(expected);
   });
 
