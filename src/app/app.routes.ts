a09d11b8454d967a7398d3f13a3b23eb301564d5
Wojciech Kwiatek <wojtek.kwiatek@gmail.com>
2017-08-08 23:26:32 +0200

    [Lazy loading] Lazy loaded modules

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { ProductsResolver } from './shared/resolvers/products.resolver';
 
+//10/ Add login with loadChildren
 export const routes: Routes = [{
   path: '',
   component: MainComponent,
   resolve: {
     productsAction: ProductsResolver
-  }
+  },
+}, {
+  path: 'login',
+  loadChildren: './login/login.module#LoginModule'
 }];
--- src/app/shared/reducers/localstorage.meta.ts
+++ src/app/shared/reducers/localstorage.meta.ts
@@ -1,6 +1,5 @@
 import { localStorageSync } from 'ngrx-store-localstorage';
-import { ActionReducer } from '@ngrx/store';
 
-export function localStorageSyncReducer(reducer: ActionReducer<any>): ActionReducer<any> {
+export function localStorageSyncReducer(reducer) {
   return localStorageSync({ keys: ['products'], rehydrate: true })(reducer);
 }
