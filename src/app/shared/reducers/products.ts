6c0dbac5526f4ea5906bee987f016c196b8dc4b8
Wojciech Kwiatek <wojtek.kwiatek@gmail.com>
2019-04-16 15:41:34 +0200

    Tasks

--- src/app/shared/reducers/products.ts
+++ src/app/shared/reducers/products.ts
@@ -17,7 +17,6 @@ export const initialState: IProduct[] = [{
 
 export default function reducer(state = initialState, action: productsActions.Actions): IProduct[] {
   switch (action.type) {
-    //2/ State change is handled here; we have to create new stated based on action and old state
     case productsActions.BUY_PRODUCT:
       return state.filter(p => p.id !== action.payload);
     default: {
