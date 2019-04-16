a476cd8b2d6b48accdb5f8fed12f2c32fdba0e5d
Wojciech Kwiatek <wojtek.kwiatek@gmail.com>
2019-04-16 12:57:10 +0200

    Project structure and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { ProductsComponent } from './main/products/products.component';
+import { ProductComponent } from './main/products/product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    ProductsComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule
